579f00983674c7446f9063e86a8bcbe4a72ee615
Prathamesh Shekade <157305410+SunnyShekade@users.noreply.github.com>
2025-05-31 19:54:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-   from flask import Flask, request, jsonify
+   from flask import Flask,request,jsonify
    from flask_cors import CORS  # Import CORS
    import your_cnn_model  # Import your CNN model here
    import logging
